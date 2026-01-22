00ea1510c15e4de36b9dd6127360b8375d270e42
Tamburini-Christofer <christofer.tamburini92@gmail.com>
2026-01-22 12:49:42 +0100

    Creazione riassunto Selettori Css

---
+++ 2.css/Selettori.js
@@ -0,0 +1,34 @@
+//todo Selettori base
+// * //* Seleziona tutti gli elementi
+// element //* Seleziona tutti gli elementi di quel tipo (es: p, h1, div)
+// .class //* Seleziona tutti gli elementi con quella classe
+// #id //* Seleziona l’elemento con quell’id
+// element, element //* Seleziona più elementi separati da virgola
+// element element //* Seleziona elementi figli discendenti di un altro elemento
+// element > element //* Seleziona elementi figli diretti
+// element + element //* Seleziona l’elemento immediatamente successivo
+// element ~ element //* Seleziona elementi fratelli successivi
+// [attribute] //* Seleziona elementi che hanno un certo attributo
+// [attribute=value] //* Seleziona elementi con attributo uguale a un valore
+// [attribute~=value] //* Seleziona elementi con attributo contenente una parola
+// [attribute|=value] //* Seleziona elementi con attributo che inizia con valore seguito da "-" 
+
+//todo Selettori avanzati
+// :hover //* Seleziona elemento al passaggio del mouse
+// :active //* Seleziona elemento mentre è cliccato
+// :focus //* Seleziona elemento con focus (input, textarea…)
+// :first-child //* Seleziona il primo figlio di un elemento
+// :last-child //* Seleziona l’ultimo figlio di un elemento
+// :nth-child(n) //* Seleziona l’ennesimo figlio (n può essere numero, formula o parola chiave)
+// :nth-of-type(n) //* Seleziona l’ennesimo figlio di un certo tipo
+// :first-of-type //* Primo figlio di un certo tipo
+// :last-of-type //* Ultimo figlio di un certo tipo
+// :not(selector) //* Seleziona elementi che non corrispondono al selettore
+// :empty //* Seleziona elementi senza figli
+// ::before //* Pseudo-elemento per inserire contenuto prima dell’elemento
+// ::after //* Pseudo-elemento per inserire contenuto dopo l’elemento
+// ::first-letter //* Pseudo-elemento per prima lettera
+// ::first-line //* Pseudo-elemento per prima riga
+// ::selection //* Seleziona il testo evidenziato
+// @media (condition) //* Selettore per media query, regola lo stile per device o dimensioni
+
